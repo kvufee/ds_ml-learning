1f166e71fe774f1266dd78f3e629db06287e566b
wibbleydock <pzns1337@gmail.com>
2022-12-08 16:48:31 +0300

    [MODIFY] minor code fixes

--- knn-reg/knn-reg.py
+++ knn-reg/knn-reg.py
@@ -6,14 +6,6 @@ from sklearn.datasets import make_regression
 from sklearn.neighbors import KNeighborsRegressor
 
 
-X, y = make_regression(n_samples=228,
-                       n_features=1,
-                       noise=20,
-                       random_state=True)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-
-
 class KNNRegressor:
     def __init__(self, k_nb: int) -> None:
         self.k_nb = k_nb
@@ -23,23 +15,29 @@ class KNNRegressor:
         self.y = y
         
     def predict(self, data:List[float]) -> List[float]:
-        predict_data = np.array([])
+        predictions_list = np.array([])
         
         for item in data:
             k_distance = np.linalg.norm(self.X - item, axis=1)
             k_indices = np.argsort(k_distance)[:self.k_nb]
 
-            predict_data = np.append(predict_data, np.mean(self.y[k_indices]))
+            predictions_list = np.append(predictions_list, np.mean(self.y[k_indices]))
             
-        return predict_data
+        return predictions_list
 
 
+if __name__ == "__main__":
+    X, y = make_regression(n_samples=228,
+                           n_features=1,
+                           noise=20,
+                           random_state=True)
 
-bebra = KNNRegressor(k_nb = 5)
-bebra.fit(X_train, y_train)
-print(bebra.predict(X_test))
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
 
+    my_classifier = KNNRegressor(k_nb = 5)
+    my_classifier.fit(X_train, y_train)
+    print(my_classifier.predict(X_test))
 
-bobik = KNeighborsRegressor(n_neighbors = 5)
-bobik.fit(X_train, y_train)
-print(bobik.predict(X_test))
+    imported_classifier = KNeighborsRegressor()
+    imported_classifier.fit(X_train, y_train)
+    print(imported_classifier.predict(X_test))
